4c8b05f6891f3e77100b08017a34e960481cdf4d
jvgsantos <jvgs@cin.ufpe.br>
2019-11-07 13:43:32 -0300

    Criação da classe basica magia
    criação da classe Magia; com os get e set.

---
+++ src/ClassesBasicas/Magia.java
@@ -0,0 +1,50 @@
+package ClassesBasicas;
+
+public class Magia {
+	private String nome;
+	private int dano;
+	private int gasto;
+	private int cura;
+	private String efeito;
+	private String tipo;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getDano() {
+		return dano;
+	}
+	public void setDano(int dano) {
+		this.dano = dano;
+	}
+	public int getGasto() {
+		return gasto;
+	}
+	public void setGasto(int gasto) {
+		this.gasto = gasto;
+	}
+	public int getCura() {
+		return cura;
+	}
+	public void setCura(int cura) {
+		this.cura = cura;
+	}
+	public String getEfeito() {
+		return efeito;
+	}
+	public void setEfeito(String efeito) {
+		this.efeito = efeito;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+	
+
+}
