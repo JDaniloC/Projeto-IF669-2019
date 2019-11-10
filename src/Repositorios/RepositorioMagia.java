8d489b20678c6b40cf9dbe6a7142777cbef8f807
jvgsantos <jvgs@cin.ufpe.br>
2019-11-10 18:22:44 -0300

    jvgs - add das exceções e criação do RespositorioArray
    foi criado o respositoria com Array e foi adicionado as exceções em ambos.

--- src/Repositorios/RepositorioMagia.java
+++ src/Repositorios/RepositorioMagia.java
@@ -1,12 +1,14 @@
 package Repositorios;
 
 import ClassesBasicas.Magia;
+import Excecoes.MagiaJaExisteException;
+import Excecoes.MagiaNaoEncontradoException;
 
 public interface RepositorioMagia {
 	void inserir(Magia mag);
-    void atualizar(Magia magia);
-    void remover(String nome);
-    Magia procurar(String nome);    
-    boolean existe(String nome);
+    void atualizar(Magia magia) throws MagiaNaoEncontradoException;
+    void remover(String nome) throws MagiaNaoEncontradoException;
+    Magia procurar(String nome) throws MagiaNaoEncontradoException;    
+    boolean existe(String nome) ;
 
 }
