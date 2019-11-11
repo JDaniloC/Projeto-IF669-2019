24b8dca088f33c90c9ff727e2fbee5d3b8219ace
Thiago Henrique Tomais de Oliveira <thto@windows.cin.ufpe.br>
2019-11-11 15:43:06 -0300

    Exception Update

--- src/Repositorios/RepositorioEquipamentoLista.java
+++ src/Repositorios/RepositorioEquipamentoLista.java
@@ -1,6 +1,8 @@
 package Repositorios;
 
 import ClassesBasicas.Equipamento;
+import Excecoes.EquipamentoJaCadastradoException;
+import Excecoes.EquipamentoNaoEncontradoException;
 
 public class RepositorioEquipamentoLista implements RepositorioEquipamento {
 	private Equipamento equipamento;
@@ -12,19 +14,26 @@ public class RepositorioEquipamentoLista implements RepositorioEquipamento {
 	}
 
 	@Override
-	public void inserir(Equipamento equipamento) {
+	public void inserir(Equipamento equipamento) throws EquipamentoJaCadastradoException {
 		if (this.proximo == null) {
 			this.equipamento = equipamento;
 			this.proximo = new RepositorioEquipamentoLista();
-		} else {
+		} 
+		else if (existe(this.equipamento.getNome())) {
+			throw new EquipamentoJaCadastradoException();
+		}
+		else {
 			this.proximo.inserir(equipamento);
 		}
 	}
 
 	@Override
-	public void remover(String nome) {
-		if (this.equipamento != null && this.equipamento.getNome() == nome) {
-			if (this.proximo != null) {
+	public void remover(String nome) throws EquipamentoNaoEncontradoException {
+		if (!existe(nome)) {
+			throw new EquipamentoNaoEncontradoException();
+		}
+		else if (this.equipamento != null && this.equipamento.getNome() == nome) {
+			 if (this.proximo != null) {
 				this.equipamento = this.proximo.equipamento;
 				this.proximo = this.proximo.proximo;
 			}
@@ -34,10 +43,9 @@ public class RepositorioEquipamentoLista implements RepositorioEquipamento {
 	}
 
 	@Override
-	public void atualizar(Equipamento equipamento) {
-
+	public void atualizar(Equipamento equipamento) throws EquipamentoNaoEncontradoException {
 		if (this.equipamento == null) {
-
+			throw new EquipamentoNaoEncontradoException();
 		} else if (this.equipamento.getNome() == equipamento.getNome()) {
 			this.equipamento = equipamento;
 		} else {
@@ -45,18 +53,18 @@ public class RepositorioEquipamentoLista implements RepositorioEquipamento {
 		}
 	}
 	@Override
-	public Equipamento procurar(String nome) {
-		if (this.equipamento != null && this.equipamento.getNome().equals(nome)) {
-			return equipamento;
-		} else if (this.proximo != null) {
-			return this.proximo.procurar(nome);
-		} else {
-			return null;
+	public Equipamento procurar(String nome) throws EquipamentoNaoEncontradoException {
+		if (this.equipamento == null) {
+			throw new EquipamentoNaoEncontradoException();
 		}
+		else if (this.equipamento.getNome().equals(nome)) {
+			return equipamento;
+		} 
+		return this.proximo.procurar(nome);
 	}
 	@Override
 	public boolean existe(String nome) {
-		if (this.procurar(nome).equals(null)) {
+		if (this.equipamento.equals(null)) {
 			return false;
 		} else {
 			return true;
