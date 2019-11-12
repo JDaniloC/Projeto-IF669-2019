695d80cdfd975f355581ae148d961fae119c6791
joaomazzarolo <41448205+joaomazzarolo@users.noreply.github.com>
2019-11-12 17:44:20 -0300

    Add files via upload
    Adição de Cidade à Fachada geral

--- src/FachadaGeral/FachadaGeral.java
+++ src/FachadaGeral/FachadaGeral.java
@@ -6,6 +6,7 @@ import Excecoes.*;
 
 public class FachadaGeral{
     private RepositorioPersonagem repPersonagem;
+    private FachadaCidade fachadaCidade;
 
     public FachadaGeral(String dados){
         String informacoes[] = dados.split(" ");
@@ -15,5 +16,65 @@ public class FachadaGeral{
     public void Adicionar(String informacoes) throws PersonagemJaExisteException{
         String[] inf = informacoes.split(" ");
         // Verifica se tem as magias, equipamentos, e adiciona.
+    }
+
+    //Cidade
+
+    public void cadastrarCidade(Cidade cidade) throws CidadeJaExisteException, CidadeNaoExisteException, PopulacaoInvalidaException, MissaoInvalidaException,
+            CidadeInvalidaException, PersonagemNaoExisteException{
+        if (cidade != null)
+        {
+            if (!fachadaCidade.existe(cidade.getCidade()))
+            {
+                if (cidade.populcao>0)
+                {
+                    if (cidade.getVendedor() != null)
+                    {
+                        if(cidade.getMissao() != null){
+                            if(cidade.getMonstro() != null){
+                                fachadaCidade.inserir(cidade);
+                            }
+                            else throw new PersonagemNaoExisteException();
+                        }
+                        else throw new MissaoInvalidaException();
+                    }
+                    else throw new CidadeInvalidaException();
+                }
+                else
+                    throw new PopulacaoInvalidaException();
+            }
+            else
+                throw new CidadeJaExisteException();
+        }
+        else
+            throw new CidadeInvalidaException();
+    }
+
+    public void removerCidade(String nome) throws CidadeInvalidaException, CidadeNaoExisteException {
+        if (nome != null)
+        {
+            if (fachadaCidade.existe(nome))
+            {
+                fachadaCidade.remover(nome);
+            }
+            else
+                throw new CidadeNaoExisteException();
+        }
+        else
+            throw new CidadeInvalidaException();
+    }
+
+    public Local procurarLocal(String nome) throws CidadeInvalidaException, CidadeNaoExisteException {
+        if (nome != null)
+        {
+            if (fachadaCidade.existe(nome))
+            {
+                return fachadaCidade.procurar(nome);
+            }
+            else
+                throw new CidadeNaoExisteException();
+        }
+        else
+            throw new CidadeInvalidaException();
     }
 }
