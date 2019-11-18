7ffde8d10df6209e90547cf6ff1e934a38f56a3e
Thiago Henrique Tomais de Oliveira <thto@WINDOWS.CIN.UFPE.BR>
2019-11-18 17:02:14 -0300

    Message

--- src/InterfaceUsuario.java
+++ src/InterfaceUsuario.java
@@ -7,23 +7,29 @@ import ClassesFachadaIndividual.*;
 public class InterfaceUsuario {
 
     public static void main(String[] args) {
-        RepositorioEquipamento repEquipamento = new RepositorioEquipamentoArray(2);
+        RepositorioEquipamento repEquipamento = new RepositorioEquipamentoArray(3);
+        RepositorioPersonagem repPersonagem = new RepositorioPersonagemArray();
+        RepositorioCidade repCidade = new RepositorioCidadeArray();
+        FachadaEquipamento equipamento = new FachadaEquipamento(repEquipamento);
+        FachadaCidade cidade = new FachadaCidade(repCidade);
+        FahcadaPersonagem personagem = new FachadaPersonagem(repPersonagem);
+        FachadaGeral programa = new FachadaGeral(personagem, equipamento, cidade);
         
         try {
             Equipamento equipamento01 = new Equipamento("TornozeleiraBerserk", 150, 0, 40, 20, "Aumenta sua Defesa e Vida");
-            equipamento.inserir(equipamento01);
+            programa.cadastrarEquipamento(equipamento01);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         try {
             Equipamento equipamento02 = new Equipamento("Gargantuilha", 350, 100, 0, 0, "Aumenta consideravelmente seu Ataque");
-            equipamento.inserir(equipamento02);
-            equipamento02.setNome("NokiaTijolao");
-            equipamento.atualizar();
+            programa.cadastrarEquipamento(equipamento02);
+            equipamento02.setAtaque(150);
+            programa.atualizarEquipamento(equipamento02);
         } catch (Exception e) {
             System.out.println(e.getMessage());
     }
         try {
-            equipamento.FachadaGeral.RepositorioEquipamento.remover("NokiaTijolao");
+            equipamento.remover("NokiaTijolao");
         }
 }
