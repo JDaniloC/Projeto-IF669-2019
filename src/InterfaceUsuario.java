373b4422356d721973c8f954f063e01f2e8bf44f
joaomazzarolo <41448205+joaomazzarolo@users.noreply.github.com>
2019-11-19 16:59:39 -0300

    Update InterfaceUsuario.java
    Adição do teste de Cidade

--- src/InterfaceUsuario.java
+++ src/InterfaceUsuario.java
@@ -29,5 +29,64 @@ public class InterfaceUsuario {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+        
+        
+        //inicio do teste de Cidade
+            try{
+                Cidade cidade1 = new Cidade("Paracetamol",1000,Equipamento equipamentoC1 = new Equipamento("Cotaspirina", 150, 0, 40, 20, "Aumenta sua Defesa e Vida"),
+                        "Mate 5 goblins",Monstro monstroC1 = new Monstro("DDC", 100, 0, 50, 10, 0, 1, null, null, null));
+                programa.cadastrarCidade(cidade1);
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+            try{
+                Cidade cidade2 = new Cidade("Cidadelonge",10,Equipamento equipamentoC2 = new Equipamento("Rina", 200, 0, 40, 20, "Aumenta sua Defesa e Vida"),
+                        null,Monstro monstroC1 = new Monstro("DDC", 100, 0, 50, 10, 0, 1, null, null, null));
+                programa.cadastrarCidade(cidade2);
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+            try{
+                Cidade cidade3 = new Cidade("Cidadeperto",0,Equipamento equipamentoC3 = new Equipamento("Cota", 150, 0, 50, 20, "Aumenta sua Defesa e Vida"),
+                        "Mate 5 lobos",Monstro monstroC1 = new Monstro("DDC", 100, 0, 50, 10, 0, 1, null, null, null));
+                programa.cadastrarCidade(cidade3);
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+            try{
+                Cidade cidade4 = new Cidade(null,2000,Equipamento equipamentoC4 = new Equipamento("Cotirina", 150, 0, 40, 20, "Aumenta sua Defesa e Vida"),
+                        "Mate 5 trolls",Monstro monstroC1 = new Monstro("DDC", 100, 0, 50, 10, 0, 1, null, null, null));
+                programa.cadastrarCidade(cidade4);
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+            try{
+                Cidade cidade5 = new Cidade("Cidadeali",500,Equipamento equipamentoC5 = new Equipamento("Coina", 150, 0, 40, 20, "Aumenta sua Defesa e Vida"),
+                        "Mate 3 goblins",Monstro monstroC1 = new Monstro("DDC", 100, 0, 50, 10, 0, 1, null, null, null));
+                programa.cadastrarCidade(cidade5);
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+            try{
+                programa.procurarLocal("Paracetamol")
+            }catch (Exception e){
+                System.out.println(e.getMessage);
+            }
+            try{
+                programa.procurarLocal("tamol")
+            }catch (Exception e){
+                System.out.println(e.getMessage);
+            }
+            try{
+                programa.removerCidade("Paracetamol")
+            }catch (Exception e){
+                System.out.println(e.getMessage);
+            }
+            try{
+                programa.procurarLocal("Paracetamol")
+            }catch (Exception e){
+                System.out.println(e.getMessage);
+            }
+            //Fim do teste de Cidade
     }
 }
