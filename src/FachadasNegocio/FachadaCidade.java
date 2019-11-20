33d3f564f7707dec3a9613d2bab798d13940680a
joaomazzarolo <41448205+joaomazzarolo@users.noreply.github.com>
2019-11-20 09:38:55 -0300

    Update FachadaCidade.java
    correção

--- src/FachadasNegocio/FachadaCidade.java
+++ src/FachadasNegocio/FachadaCidade.java
@@ -11,38 +11,67 @@ import Repositorios.RepositorioCidadeLista;
 public class FachadaCidade{
     private RepositorioCidade repositorioCidade;
 
-    // Verificação para definir o tipo de repositório a ser usado
-    public FachadaCidade(RepositorioCidade cidade) {
-        repositorioCidade = cidade;
-    }
-
     // Verificação de existência da cidade
     public boolean existe(String nome){ return repositorioCidade.existe(nome);}
 
-    public void inserir(Cidade cidade) throws CidadeJaExisteException{
-        if(!existe(cidade.getCidade())){
-            repositorioCidade.inserir(cidade);
-        }else{
-            throw new CidadeJaExisteException();
+    public void inserir(Cidade cidade) throws CidadeJaExisteException, CidadeInvalidaException, PopulacaoInvalidaException, CidadeInvalidaException,
+            MissaoInvalidaException, PersonagemNaoExisteException{
+        if (cidade != null)
+        {
+            if (!repositorioCidade.existe(cidade.getCidade()))
+            {
+                if (cidade.getPopulacao()>0)
+                {
+                    if (cidade.getVendedor() != null)
+                    {
+                        if(cidade.getMissao() != null){
+                            if(cidade.getMonstro() != null){
+                                repositorioCidade.inserir(cidade);
+                            }
+                            else throw new PersonagemNaoExisteException();
+                        }
+                        else throw new MissaoInvalidaException();
+                    }
+                    else throw new CidadeInvalidaException();
+                }
+                else
+                    throw new PopulacaoInvalidaException();
+            }
+            else
+                throw new CidadeJaExisteException();
         }
+        else
+            throw new CidadeInvalidaException();
     }
 
-    // Procura uma cidade caso exista
-    public Cidade procurar(String nome) throws CidadeNaoExisteException{
-        if(existe(nome)){
-            return repositorioCidade.procurar(nome);
-        }else{
-            throw new CidadeNaoExisteException();
+ // Procura uma cidade caso exista
+    public Cidade procurar(String nome) throws CidadeNaoExisteException, CidadeInvalidaException{
+        if (nome != null)
+        {
+            if (repositorioCidade.existe(nome))
+            {
+                return repositorioCidade.procurar(nome);
+            }
+            else
+                throw new CidadeNaoExisteException();
         }
+        else
+            throw new CidadeInvalidaException();
     }
 
-    // Remove uma cidade do repositório
-    public void remover(String nome) throws CidadeNaoExisteException{
-        if(existe(nome)){
-            repositorioCidade.remover(nome);
-        }else{
-            throw new CidadeNaoExisteException();
+ // Remove uma cidade do repositório
+    public void remover(String nome) throws CidadeNaoExisteException, CidadeInvalidaException{
+        if (nome != null)
+        {
+            if (repositorioCidade.existe(nome))
+            {
+                repositorioCidade.remover(nome);
+            }
+            else
+                throw new CidadeNaoExisteException();
         }
+        else
+            throw new CidadeInvalidaException();
     }
 
 }
